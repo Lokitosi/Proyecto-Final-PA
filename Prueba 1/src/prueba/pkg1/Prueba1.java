300ec6e62d97cc9ff21522745016a1cb355f8a40
Lokitosi10 <50706643+Lokitosi10@users.noreply.github.com>
2020-08-13 14:25:56 -0500

    realizadas pruebas

---
+++ Prueba 1/src/prueba/pkg1/Prueba1.java
@@ -0,0 +1,12 @@
+
+package prueba.pkg1;
+
+
+public class Prueba1 {
+
+    public static void main(String[] args) {
+        System.out.println("jajajajaja");
+        System.out.println("si edito aca y luego le doy a guardar lo hace, que bien :)");
+    }
+    
+}

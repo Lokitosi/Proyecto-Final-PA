3f77e21f98ee7c7188ec7760862f99e65e613c71
Lokitosi10 <50706643+Lokitosi10@users.noreply.github.com>
2020-08-25 13:47:51 -0500

    ahora si completamente funcional

--- Biblioteca2/src/java/Entidad/Libro.java
+++ Biblioteca2/src/java/Entidad/Libro.java
@@ -132,7 +132,7 @@ public class Libro implements Serializable {
 
     @Override
     public String toString() {
-        return "Entidad.Libro[ id=" + id + " ]";
+        return "Nombre= "+libTitulo+"‎‎‎‎‎‎‎‎‎‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎‏‏‎ ‎‏‏‎ ‎‏‏‎‏‏‎ ‎‏‏‎ ‎‏‏‎‏‏‎ ‎‏‏‎ ‎‏‏‎"+"id= "+id+"‎‎‎‎‎‎‎‎‎‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎‏‏‎ ‎‏‏‎ ‎‏‏‎‏‏‎ ‎‏‏‎ ‎‏‏‎‏‏‎ ‎‏‏‎ ‎‏‏‎"+"precio= "+libPrecio+"<hr>";
     }
     
 }
